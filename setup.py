c52d3662a6f62c202a00f69fa6612e397a11b97c
IanWernecke <IanWernecke@protonmail.com>
2019-02-02 06:42:47 -0500

    Updated the version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='exul',
-    version='0.0.2',
+    version='0.0.3',
     author='IanWernecke',
     author_email='IanWernecke@protonmail.com',
     description='A package for handling Xlib operations in an easy-to-use fashion.',
